fa3775a388a8d5270fa6218c747cb0e60c9d3e96
EC2 Default User <ec2-user@ip-172-31-14-205.ap-southeast-2.compute.internal>
2024-06-27 13:55:36 +0000

    修正9

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,22 +1,25 @@
 class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
   def top
+    @reported_posts_per_page = 5
+    @reported_comments_per_page = 5
+
     # 通報された投稿
     @reported_posts = Post.joins(:reports).distinct
     @reported_posts_count = @reported_posts.count
-    @reported_posts_per_page = 5
-    @reported_posts = @reported_posts.offset(offset).limit(@reported_posts_per_page)
+    @reported_posts = @reported_posts.offset(offset(@reported_posts_per_page)).limit(@reported_posts_per_page)
+
     # 通報されたコメント
     @reported_comments = Comment.joins(:reports).distinct
     @reported_comments_count = @reported_comments.count
-    @reported_comments_per_page = 5
-    @reported_comments = @reported_comments.offset(offset).limit(@reported_comments_per_page)
+    @reported_comments = @reported_comments.offset(offset(@reported_comments_per_page)).limit(@reported_comments_per_page)
   end
-end
+
 
 private
 
 # ページネーション
-def offset
-  (params[:page].to_i - 1) * @reported_posts_per_page
+  def offset(items_per_page)
+    [(params[:page].to_i - 1) * items_per_page, 0].max
+  end
 end
