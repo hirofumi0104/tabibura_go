02c301dbb23c29fdf207c044480abcf7799c5db1
EC2 Default User <ec2-user@ip-172-31-14-205.ap-southeast-2.compute.internal>
2024-07-05 14:12:21 +0000

    通知機能修正です

--- db/migrate/20240617114659_create_notifications.rb
+++ db/migrate/20240617114659_create_notifications.rb
@@ -3,7 +3,7 @@ class CreateNotifications < ActiveRecord::Migration[6.1]
     create_table :notifications do |t|
       t.references :user, null: false, foreign_key: true
       t.references :post, null: false, foreign_key: true
-      t.integer :comment_id, null: true
+      t.integer :comment_id, null: true, default: nil
       t.boolean :read, null: false, default: false 
       
       t.timestamps
