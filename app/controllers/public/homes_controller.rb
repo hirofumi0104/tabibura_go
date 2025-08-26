d5bbc9fbf68ba1508fab7dd93fbb05f53f47b045
kitaura1107 <hiro010433@yahoo.co.jp>
2025-08-26 23:34:32 +0900

    My SQLの記述変更

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -3,11 +3,7 @@ class Public::HomesController < ApplicationController
     # 最新の投稿を4件取得、@postsに
     @posts =Post.published.order('id DESC').limit(4)
     # ランダムな投稿を4件取得、@recommended_postsに
-    if Rails.env.production?  # 本番環境の場合
-      @recommended_posts = Post.published.order("RAND()").limit(4)
-    else
-      @recommended_posts = Post.published.order("RANDOM()").limit(4)
-    end
+    @recommended_posts = Post.published.order("RANDOM()").limit(4)
   end
 
   def about
